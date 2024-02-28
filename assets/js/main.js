8cd4dfa8829ca2666de60ed4836430750119f4d0
Artur Pedro <arturpedromartins@gmail.com>
2024-02-28 14:43:22 -0300

    feat: responsive design and folder tree refactored

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function fadein(elem) {
     elem.classList.replace('faded','fadein');
-}
+    }
 function typeWriter(elementId, text, speed, i = 0) {
     // Ensure 'i' is initialized with a default value of 0 on the first call
     if (i < text.length) {
@@ -12,3 +12,28 @@ function typeWriter(elementId, text, speed, i = 0) {
         }, speed);
     }
 }
+
+function toggleVisibility() {
+var dropElement = document.getElementById('side-bar-mobile');
+var isChecked = document.getElementById('toggle-menu').checked;
+var screenWidth = window.innerWidth;
+
+if (isChecked && screenWidth <= 700) {
+    dropElement.style.display = 'block'; // Make sure the element is visible before dropping it
+    requestAnimationFrame(() => {
+    dropElement.style.top = '0'; // Drop down
+    });
+} else {
+    dropElement.style.top = '-100%'; // Start moving it off-screen
+    setTimeout(() => {
+    dropElement.style.display = 'none'; // Hide after transition
+    }, 500);
+}
+}
+
+var date = new Date;
+document.getElementById('date').innerHTML = "&copy; " + date.getFullYear() + " - Artur Pedro";
+// Event listener for checkbox changes
+document.getElementById('toggle-menu').addEventListener('change', toggleVisibility);
+// Event listener for window resize
+window.addEventListener('resize', toggleVisibility); 
