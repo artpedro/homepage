12fca82c192d099f135214fb16b5fc4572e3122e
Artur Pedro <arturpedromartins@gmail.com>
2024-02-28 16:21:11 -0300

    fix: format and change sidebar color

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,9 @@ function fadein(elem) {
     elem.classList.replace('faded','fadein');
     }
 function typeWriter(elementId, text, speed, i = 0) {
-    // Ensure 'i' is initialized with a default value of 0 on the first call
     if (i < text.length) {
         document.getElementById(elementId).innerHTML += text.charAt(i);
         i++;
-        // Call the function again for the next character, after a delay
         setTimeout(function() {
             typeWriter(elementId, text, speed, i);
         }, speed);
@@ -19,21 +17,19 @@ var isChecked = document.getElementById('toggle-menu').checked;
 var screenWidth = window.innerWidth;
 
 if (isChecked && screenWidth <= 700) {
-    dropElement.style.display = 'block'; // Make sure the element is visible before dropping it
+    dropElement.style.display = 'flex';
     requestAnimationFrame(() => {
-    dropElement.style.top = '0'; // Drop down
+    dropElement.style.top = '0';
     });
 } else {
-    dropElement.style.top = '-100%'; // Start moving it off-screen
+    dropElement.style.top = '-100%';
     setTimeout(() => {
-    dropElement.style.display = 'none'; // Hide after transition
+    dropElement.style.display = 'none';
     }, 500);
 }
 }
 
 var date = new Date;
 document.getElementById('date').innerHTML = "&copy; " + date.getFullYear() + " - Artur Pedro";
-// Event listener for checkbox changes
 document.getElementById('toggle-menu').addEventListener('change', toggleVisibility);
-// Event listener for window resize
 window.addEventListener('resize', toggleVisibility); 
